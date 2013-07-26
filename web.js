12e60a0074c2ded1e6db694d70647e0c64968c59
Light Yagami <kira2273@gmail.com>
2013-07-26 14:17:21 +0000

    read index.html file and print that out

--- web.js
+++ web.js
@@ -1,9 +1,14 @@
 var express = require('express');
+var fs = require('fs');
 var app = express();
 app.use(express.logger());
 
 app.get('/', function(request, response) {
-  response.send('Hello World2!');
+  var data = fs.readFileSync('index.html');
+  var buf  = new Buffer(128)
+  buf.write(data); 
+  response.send(buf.toString());
+  //response.send('Hello World2!');
 });
 
 var port = process.env.PORT || 5000;
